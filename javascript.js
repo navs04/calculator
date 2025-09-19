6662045c456037d4e50fc2651c62ff0618d1c3b9
Navya Bhatia <navyabhatia48@gmail.com>
2025-09-19 15:12:34 +0530

    worked on functioning of calculator

--- javascript.js
+++ javascript.js
@@ -14,25 +14,22 @@ function divide(a,b){
     return a/b;
 }
 
-let number1;
-let number2;
-let operator;
-
-function operate(op,num1,num2){
-    if(op=='+'){
-        add(num1,num2);
+let result;
+function operate(opr,num1,num2){
+    if(opr=='+'){
+        result=add(num1,num2);
     }
-    else if(op=='-'){
-        subtract(num1,num2);
+    else if(opr=='-'){
+        result=subtract(num1,num2);
     }
-    else if(op=='*'){
-        multiply(num1,num2);
+    else if(opr=='*'){
+        result=multiply(num1,num2);
     }
-    else if(op='/'){
-        divide(num1,num2);
+    else if(opr='/'){
+        result=divide(num1,num2);
     }
     else{
-        return 'invalid operator';
+        result='invalid operator';
     }
 }
 
@@ -40,7 +37,6 @@ div= document.querySelector('#calc-display');
 
 display=document.createElement('button');
 display.classList.add('display');
-display.textContent='1+4=5';
 div.appendChild(display);
 
 div1=document.createElement('div')
@@ -50,23 +46,10 @@ div2.classList.add('num2');
 op1=document.createElement('div');
 op1.classList.add('op');
 
-for(let i=0;i<10;i++){
-    if(i<5){
-        let num=document.createElement('button');
-        num.classList.add('num');
-        num.textContent=i; 
-        div1.appendChild(num);
-    }
-    else{
-        let num=document.createElement('button');
-        num.classList.add('num');
-        num.textContent=i;
-        div2.appendChild(num);
-    }
-}
-
-div.appendChild(div1);
-div.appendChild(div2);
+let number1=[];
+let number2=[];
+let operator=[];
+let operatorClicked=false;
 
 for(let i=0;i<5;i++){
     let arr=['+','-','*','/','='];
@@ -74,12 +57,75 @@ for(let i=0;i<5;i++){
     let op=document.createElement('button');
     op.classList.add('operator');
     op.textContent=arr[i];
+
+    op.addEventListener('click',function(e){
+        operatorClicked=true;
+        operator.push(op.textContent);
+
+        if(op.textContent=='='){
+            operate(operator[0],number1.join(''),number2.join(''));
+            display.textContent=result;
+        }
+        else{
+            display.textContent=operator[0];
+        }
+    })
+
     op1.appendChild(op);
 }
 
 div.appendChild(op1);
 
+for(let i=0;i<10;i++){
+    if(i<5){
+        let num=document.createElement('button');
+        num.classList.add('num');
+        num.textContent=i;
+        div1.appendChild(num);
+
+        num.addEventListener('click',function(){
+            if(operatorClicked==true){
+                number2.push(num.textContent);
+                display.textContent=number2.join('');
+            }
+            else{
+                number1.push(num.textContent);
+                display.textContent=number1.join('');
+            }
+        })
+    }
+    else{
+        let num=document.createElement('button');
+        num.classList.add('num');
+        num.textContent=i;
+        div2.appendChild(num);
+
+        num.addEventListener('click',function(){
+            if(operatorClicked==true){
+                number2.push(num.textContent);
+                display.textContent=number2.join('');
+            }
+            else{
+                number1.push(num.textContent);
+                display.textContent=number1.join('');
+            }
+        }) 
+    }
+}
+
+div.appendChild(div1);
+div.appendChild(div2);
+
 clearBtn=document.createElement('button');
 clearBtn.classList.add('clear');
 clearBtn.textContent='CE';
+
 div.appendChild(clearBtn);
+
+clearBtn.addEventListener('click',function(){
+    display.innerHTML="";
+    number1=[];
+    number2=[];
+    operator=[];
+})
+
